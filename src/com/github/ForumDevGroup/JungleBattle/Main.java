dddf0f716aa35bc0e21ecf9bd09ce55d7f4c00be
Flo // Sheigutn <fb020198@gmail.com>
2014-08-26 11:41:37 +0200

    Added eclipe files to gitignore, create Main.java

---
+++ src/com/github/ForumDevGroup/JungleBattle/Main.java
@@ -0,0 +1,13 @@
+package com.github.ForumDevGroup.JungleBattle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	@Override
+	public void onEnable() {
+	}
+
+	@Override
+	public void onDisable() {
+	}
+}
