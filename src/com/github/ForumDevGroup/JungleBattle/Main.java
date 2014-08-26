a774ba5de608406bd23f9ac860602525d6a9a3f3
Zeno <devcn0@gmail.com>
2014-08-26 20:36:28 +0200

    Hauptklasse angefangen

--- src/com/github/ForumDevGroup/JungleBattle/Main.java
+++ src/com/github/ForumDevGroup/JungleBattle/Main.java
@@ -3,11 +3,23 @@ package com.github.ForumDevGroup.JungleBattle;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+
+    // Instanz der Hauptklasse
+    private static Main instance;
+
 	@Override
 	public void onEnable() {
+        // Hauptklasse wird an die Instanz übergeben
+        this.instance = this;
+        /*
+         * Hier kommt noch das Zeug rein :)
+         */
+        this.getLogger().info("Loaded plugin successfully.");
 	}
 
 	@Override
 	public void onDisable() {
+        this.getLogger().info("Stopped plugin successfully.");
 	}
+
 }
