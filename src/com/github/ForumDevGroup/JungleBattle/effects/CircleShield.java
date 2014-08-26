63a7458d52d2819c984b939520244639282527ba
Flo // Sheigutn <fb020198@gmail.com>
2014-08-26 22:20:53 +0200

    Effects Ordner hinzugefügt, 1. Effect ebenso

---
+++ src/com/github/ForumDevGroup/JungleBattle/effects/CircleShield.java
@@ -0,0 +1,30 @@
+package com.github.ForumDevGroup.JungleBattle.effects;
+
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class CircleShield extends CustomEffect {
+	@Override
+	public void runEffect(Player p) {
+		Location l = p.getEyeLocation().clone(); //Klone EyeLocation des Spielers
+		Location startLoc = p.getLocation(); //Location für Startpunkt
+		startLoc.setY(startLoc.getY() + 1.8); //deklariere Startlocation
+		double factor = 0; //Faktor für Sin / Cos Berechnung
+		boolean water = true; //Wasser Boolean Variable
+		while (factor <= 1D) { //während Faktor kleiner oder gleich 1 ist, führe Folgendes aus:
+			double xfac = Math.sin(Math.toRadians(360 * factor)); //Berechne X-Koordinaten-Faktor des Effekts
+			double zfac = Math.cos(Math.toRadians(360 * factor)); //Berechne Z-Koordinaten-Faktor des Effekts
+			l.setX(startLoc.getX() + xfac); //Addiere Faktor hinzu
+			l.setZ(startLoc.getZ() + zfac); //Ebenso
+			for (double y = 0; y < 1.8; y += 0.2) { //9-malige Ausführung
+				l.setY(startLoc.getY() - y); //Subtrahiere y-Faktor
+				if(water) p.getWorld().playEffect(l, Effect.WATERDRIP, 0); //Selbsterklärend
+				else p.getWorld().playEffect(l, Effect.LAVADRIP, 0);
+			}
+			l.setY(startLoc.getY()); //Setze Y wieder zu der des Kopfes
+			water = !water; //Drehe water Variable um
+			factor = factor + (1D / 60D); //Addiere 1/60 hinzu (60 Reihen von Drops um den Spieler herum)
+		}
+	}
+}
---
+++ src/com/github/ForumDevGroup/JungleBattle/effects/CustomEffect.java
@@ -0,0 +1,7 @@
+package com.github.ForumDevGroup.JungleBattle.effects;
+
+import org.bukkit.entity.Player;
+
+public abstract class CustomEffect {
+	public abstract void runEffect(Player p);
+}
