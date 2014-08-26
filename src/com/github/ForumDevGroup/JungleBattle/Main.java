a6d1c37d589a8f25ba2b7451b47df010c610a02f
Zeno <devcn0@gmail.com>
2014-08-26 22:52:47 +0200

    +Datenbank wird mit Daten aus der Default-Config geladen +Default-Config wird geladen

--- src/com/github/ForumDevGroup/JungleBattle/Main.java
+++ src/com/github/ForumDevGroup/JungleBattle/Main.java
@@ -1,22 +1,37 @@
 package com.github.ForumDevGroup.JungleBattle;
 
+import com.github.ForumDevGroup.JungleBattle.util.MySQL;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.comphenix.protocol.ProtocolLibrary;
 import com.github.ForumDevGroup.JungleBattle.network.LanguageAdapter;
 
+import java.sql.SQLException;
+
 public class Main extends JavaPlugin {
 
     // Instanz der Hauptklasse
     private static Main instance;
+    // MySQL Instanz
+    private static MySQL mySQL;
 
 	@Override
 	public void onEnable() {
         // Hauptklasse wird an die Instanz übergeben
         instance = this;
-        /*
-         * Hier kommt noch das Zeug rein :)
-         */
+        // Default Config wird geladen
+        this.saveDefaultConfig();
+        this.getLogger().info("Loaded default config.");
+        // Neue Instanz von MySQL wird erstellt
+        try {
+            mySQL = loadDBConnFromCfg();
+            this.getLogger().info("Connected to database.");
+        } catch(SQLException e) {
+            this.getLogger().warning("Could not connect to database.");
+            e.printStackTrace();
+        }
+        // Missing comment??
         ProtocolLibrary.getProtocolManager().addPacketListener(new LanguageAdapter());
         this.getLogger().info("Loaded plugin successfully.");
 	}
@@ -30,5 +45,16 @@ public class Main extends JavaPlugin {
 	public static Main instance() {
 		return instance;
 	}
-	
+
+    // Funktion zum laden einer neuen MySQL Instanz
+    private MySQL loadDBConnFromCfg() throws SQLException {
+        FileConfiguration cfg = this.getConfig();
+        return new MySQL(
+                cfg.getString("db.host"),
+                cfg.getString("db.dbname"),
+                cfg.getString("db.username"),
+                cfg.getString("db.password")
+        );
+    }
+
 }
