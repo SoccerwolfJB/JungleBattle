eb7d75fa8253d0b10ec4f603024bb4c1515a8392
Maxthat <maximiliantraenkner@gmail.com>
2014-08-28 17:14:01 +0200

    Adventure GameMode beim Join

--- src/com/github/ForumDevGroup/JungleBattle/listeners/TestListener.java
+++ src/com/github/ForumDevGroup/JungleBattle/listeners/TestListener.java
@@ -2,15 +2,19 @@ package com.github.ForumDevGroup.JungleBattle.listeners;
 
 import com.github.ForumDevGroup.JungleBattle.handlers.GameHandler;
 import com.github.ForumDevGroup.JungleBattle.handlers.UUIDHandler;
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+
+import static org.bukkit.GameMode.ADVENTURE;
 
 public class TestListener implements Listener {
 
     @EventHandler
     public void onEvent(final PlayerJoinEvent event) {
         UUIDHandler.update(event.getPlayer());
+        event.getPlayer().setGameMode(ADVENTURE);
     }
 
 }
