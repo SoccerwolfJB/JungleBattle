9035c07f38b5938f15717f3e6d57d48666c6eed3
Flo // Sheigutn <fb020198@gmail.com>
2014-08-28 23:03:54 +0200

    Path Annotation für Runtime angepasst

--- src/com/github/ForumDevGroup/JungleBattle/annotations/Path.java
+++ src/com/github/ForumDevGroup/JungleBattle/annotations/Path.java
@@ -1,5 +1,9 @@
 package com.github.ForumDevGroup.JungleBattle.annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Path {
 
     public String getPath();
